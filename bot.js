326b7e73137822ba818ca548fc1c7c2026562bc6
Eric Tendian <erictendian@gmail.com>
2014-12-25 16:37:55 -0600

    Added additional logging for startup of bot

--- bot.js
+++ bot.js
@@ -7,13 +7,16 @@ var T = null;
 start();
 
 function start() {
+    console.log('Initializing bot...');
     initConfig();
 }
 
 function initConfig() {
+    console.log('Initializing config...');
     fs.readFile('config.json', 'utf-8', function (err, fileContents) {
         var config = null;
         if (err) {
+            console.error(err);
             // use environment variables
             config = {
                 "consumer_key": proc.env.CONSUMER_KEY,
@@ -29,6 +32,7 @@ function initConfig() {
 }
 
 function initTwit(config) {
+    console.log('Initializing Twitter API...');
     T = new Twit({
         consumer_key:         config.consumer_key,
         consumer_secret:      config.consumer_secret,
@@ -39,9 +43,14 @@ function initTwit(config) {
 }
 
 function listen(T) {
+    console.log('Initializing bot data and starting stream...');
     fs.readFile('onlinestreams.json', 'utf-8', function (err, fileContents) {
+        if (err) console.error(err);
+
         livestreams = JSON.parse(fileContents)
         var stream = T.stream('user', {});
+
+        console.log('Bot initialized successfully. Now streaming.');
 
         stream.on('tweet', function (tweet) {
             checkTweet(tweet, livestreams);
@@ -63,6 +72,7 @@ function retweet(tweet, channel) {
         status: statusupdate,
         in_reply_to_status_id: tweet.id
     }, function (err, data, response) {
+        if (err) console.error(err);
         console.log(statusupdate);
     });
 }
